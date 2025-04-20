2134370928552dcde63c37eca658a886e6a050e1
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2025-04-20 08:10:28 -0400

    Update admin-redesign-exploration.php

--- admin-redesign-exploration.php
+++ admin-redesign-exploration.php
@@ -14,7 +14,7 @@
  */
 
 // Useful global constants.
-define( 'ADMIN_REDESIGN_VERSION', '0.1.2' );
+define( 'ADMIN_REDESIGN_VERSION', '0.1.3' );
 define( 'ADMIN_REDESIGN_URL', plugin_dir_url( __FILE__ ) );
 define( 'ADMIN_REDESIGN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'ADMIN_REDESIGN_INC', ADMIN_REDESIGN_PATH . 'includes/' );
